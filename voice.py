ba96eb00a1f3ce7833c88945ee690bef2772313a
Anthony Bird <birdas273@gmail.com>
2021-02-23 22:59:05 -0500

    define function added

--- voice.py
+++ voice.py
@@ -5,10 +5,11 @@ import subprocess
 from googlesearch import search
 import wolframalpha
 import spacy
+import wikipediaapi
 client = wolframalpha.Client('2V87UU-VJHEETLRQE')
 
 
-def main():
+def speech_recog():
     r = sr.Recognizer()
     r.energy_threshold=1000
 
@@ -22,25 +23,43 @@ def main():
         audio = r.listen(source)
 
 
-
     #Attempt a recognition
     try:
         words = r.recognize_google(audio)
     except: 
         print("Recognition failed.")
 
-
-
-    #Activate skill
     words = words.lower()
-    print("You said: " + words)
-    sep_words = words.split(' ')
+    #print("You said: " + words)
+    #sep_words = words.split(' ')
+    return words
 
+
+def define(words):
     nlp = spacy.load("en_core_web_sm")
     doc = nlp(words)
-    print("Nouns:", [chunk.text for chunk in doc.noun_chunks])
-    print("Verbs:", [token.lemma_ for token in doc if token.pos_ == "VERB"])
-    
+    nouns = [chunk.text for chunk in doc.noun_chunks]
+    verbs = [token.lemma_ for token in doc if token.pos_ == "VERB"]
+    #print("Nouns:", nouns)
+    #print("Verbs:", verbs)
 
+
+    for noun in nouns:
+        if ' and ' in noun:
+            arr = noun.split(' and ')
+            for i in arr:
+                nouns.append(i)
+            continue
+        wiki_wiki = wikipediaapi.Wikipedia('en')
+        page_py = wiki_wiki.page(noun)
+        index = page_py.summary.find('.')
+        print(noun.upper() + ': ' + page_py.summary[:index] + '.\n')
+
+
+def main():
+    #words = speech_recog()
+    words = 'i am taking classes on cryptography and graph theory on tuesdays.'
+    define(words)
+    
 
 main()
