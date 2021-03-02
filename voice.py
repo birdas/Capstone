b0504d387e6a84db1078def6f023ccfff0fd6ad8
birdas <birdas273@gmail.com>
2021-03-02 12:25:11 -0500

    ready for first in-class test

--- voice.py
+++ voice.py
@@ -3,25 +3,25 @@ import webbrowser
 import os
 import subprocess
 from googlesearch import search
-import wolframalpha
 import spacy
 import wikipediaapi
-client = wolframalpha.Client('2V87UU-VJHEETLRQE')
+import soundcard as sc
+r = sr.Recognizer()
+r.energy_threshold=1000
+nlp = spacy.load("en_core_web_sm")
 
 
 def speech_recog():
-    r = sr.Recognizer()
-    r.energy_threshold=1000
-
+    
+    words = ''
     #TESTING PURPOSES ONLY
     #for index, name in enumerate(sr.Microphone.list_microphone_names()):
         #print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
 
     #Initialize Microphone
     with sr.Microphone() as source:
-        print("Say anything: ")
+        #print("Say anything: ")
         audio = r.listen(source)
-
 
     #Attempt a recognition
     try:
@@ -30,19 +30,17 @@ def speech_recog():
         print("Recognition failed.")
 
     words = words.lower()
-    #print("You said: " + words)
+    print("You said: " + words)
     #sep_words = words.split(' ')
     return words
 
 
 def define(words):
-    nlp = spacy.load("en_core_web_sm")
     doc = nlp(words)
     nouns = [chunk.text for chunk in doc.noun_chunks]
     verbs = [token.lemma_ for token in doc if token.pos_ == "VERB"]
-    #print("Nouns:", nouns)
-    #print("Verbs:", verbs)
-
+    print("Nouns:", nouns)
+    print("Verbs:", verbs)
 
     for noun in nouns:
         if ' and ' in noun:
@@ -50,6 +48,12 @@ def define(words):
             for i in arr:
                 nouns.append(i)
             continue
+        if 'that ' in noun:
+            arr = noun.split('that ')
+            for i in arr:
+                nouns.append(i)
+            continue
+
         wiki_wiki = wikipediaapi.Wikipedia('en')
         page_py = wiki_wiki.page(noun)
         index = page_py.summary.find('.')
@@ -57,9 +61,10 @@ def define(words):
 
 
 def main():
-    #words = speech_recog()
-    words = 'i am taking classes on cryptography and graph theory on tuesdays.'
-    define(words)
-    
+    while (True):
+        words = speech_recog()
+        #words = 'i am taking classes on cryptography and graph theory on tuesdays.'
+        define(words)
+        #test()
 
 main()
